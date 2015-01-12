ff90a570ff3321d7a7e259fad8ae570aeb96e03f
Berend Schotanus <BS@BSchotanus.nl>
2015-01-12 12:13:51 +0100

    Initial Commit

---
+++ FlamingoModel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlamingoModel
+//
+//  Created by Berend Schotanus on 12-01-15.
+//  Copyright (c) 2015 First Flamingo Enterprise B.V. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FlamingoModel/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FlamingoModel
+//
+//  Created by Berend Schotanus on 12-01-15.
+//  Copyright (c) 2015 First Flamingo Enterprise B.V. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
